9fc26bbba2746af0696cba73ecec47e13d89b8fc
Dipansh Parmar <dipanshparmar870@gmail.com>
2021-10-12 15:55:35 +0530

    added expansion tile for moves

--- lib/widgets/move_tile.dart
+++ lib/widgets/move_tile.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class MoveTile extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MoveTile extends StatefulWidget {
   const MoveTile({
     Key? key,
     required this.moveAndUrl,
@@ -9,21 +12,76 @@ class MoveTile extends StatelessWidget {
 
   final Map moveAndUrl;
   final int index;
+
+  @override
+  State<MoveTile> createState() => _MoveTileState();
+}
+
+class _MoveTileState extends State<MoveTile> {
+  // this will hold the future
+  late Future _future;
+
+  // method to get the info about a move
+  Future<String> getMoveInfo() async {
+    // making the get request
+    final http.Response response = await http.get(
+      Uri.parse(widget.moveAndUrl['move']['url']),
+    );
+
+    // decoding the response
+    final decodedData = jsonDecode(response.body);
+
+    // returning the move info
+    return decodedData['effect_entries'][0]['effect'];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // assigning the future
+    _future = getMoveInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ExpansionTile(
+      title: Text(
+        widget.moveAndUrl['move']['name'],
+      ),
       children: [
-        ListTile(
-          title: Text(
-            moveAndUrl['move']['name'],
-          ),
-          trailing: Icon(
-            Icons.arrow_forward_ios,
-            color: Theme.of(context).primaryColor,
-            size: 15,
-          ),
+        FutureBuilder(
+          future: _future,
+          builder: (context, snapshot) {
+            // if waiting
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              );
+            }
+
+            // if loaded
+            if (snapshot.hasError) {
+              // if error
+              return const Center(
+                child: Text('err'), // TODO: UPDATE THIS
+              );
+            } else {
+              // if no error
+              return Text(snapshot.data as String);
+            }
+          },
         ),
       ],
+      iconColor: Theme.of(context).primaryColor,
+      textColor: Theme.of(context).primaryColor,
+      childrenPadding: const EdgeInsets.all(20),
     );
   }
 }
