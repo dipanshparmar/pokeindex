94a16a434fa24fb3a387203ba6cea80c765fbe8e
Dipansh Parmar <dipanshparmar870@gmail.com>
2021-10-22 21:40:55 +0530

    added try-catch to async await

--- lib/providers/pokemon_provider.dart
+++ lib/providers/pokemon_provider.dart
@@ -21,107 +21,103 @@ class PokemonProvider with ChangeNotifier {
 
   // method to fetch pokemon names and URLs
   Future<void> fetchPokemons() async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=-1'),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=-1'),
+      );
 
-    // decoding the response
-    final Map decodedResponse = jsonDecode(response.body);
+      // decoding the response
+      final Map decodedResponse = jsonDecode(response.body);
 
-    // getting the results
-    final List results = decodedResponse['results'];
+      // getting the results
+      final List results = decodedResponse['results'];
 
-    // emptying the names
-    _pokemonsNames.clear();
+      // emptying the names
+      _pokemonsNames.clear();
 
-    // assigning the names to the _pokemonsName
-    for (int i = 0; i < results.length; i++) {
-      _pokemonsNames.add(results[i]['name']);
+      // assigning the names to the _pokemonsName
+      for (int i = 0; i < results.length; i++) {
+        _pokemonsNames.add(results[i]['name']);
+      }
+    } catch (e) {
+      return Future.error(e);
     }
   }
 
   // method to fetch the data for a specific pokemon
   Future<void> fetchPokemonData(String name) async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse('https://pokeapi.co/api/v2/pokemon/$name'),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse('https://pokeapi.co/api/v2/pokemon/$name'),
+      );
 
-    // decoding the data
-    final Map decodedData = jsonDecode(response.body);
+      // decoding the data
+      final Map decodedData = jsonDecode(response.body);
 
-    // getting the id
-    final int id = decodedData['id'];
+      // getting the id
+      final int id = decodedData['id'];
 
-    // getting the image url
-    final String? imageUrl =
-        decodedData['sprites']['other']['official-artwork']['front_default'];
+      // getting the image url
+      final String? imageUrl =
+          decodedData['sprites']['other']['official-artwork']['front_default'];
 
-    // getting the hp
-    final int hp = decodedData['stats'][0]['base_stat'];
+      // getting the hp
+      final int hp = decodedData['stats'][0]['base_stat'];
 
-    // getting the attack
-    final int attack = decodedData['stats'][1]['base_stat'];
+      // getting the attack
+      final int attack = decodedData['stats'][1]['base_stat'];
 
-    // getting the defense
-    final int defense = decodedData['stats'][2]['base_stat'];
+      // getting the defense
+      final int defense = decodedData['stats'][2]['base_stat'];
 
-    // getting the special attack
-    final int specialAttack = decodedData['stats'][3]['base_stat'];
+      // getting the special attack
+      final int specialAttack = decodedData['stats'][3]['base_stat'];
 
-    // getting the special defense
-    final int specialDefense = decodedData['stats'][4]['base_stat'];
+      // getting the special defense
+      final int specialDefense = decodedData['stats'][4]['base_stat'];
 
-    // getting the speed
-    final int speed = decodedData['stats'][5]['base_stat'];
+      // getting the speed
+      final int speed = decodedData['stats'][5]['base_stat'];
 
-    // getting the weight
-    final int weight = getWeightInKg(
-      decodedData['weight'],
-    ); // default to hectogram
+      // getting the weight
+      final int weight = getWeightInKg(
+        decodedData['weight'],
+      ); // default to hectogram
 
-    // getting the abilites
-    final List abilities = decodedData['abilities'];
+      // getting the abilites
+      final List abilities = decodedData['abilities'];
 
-    // getting the moves
-    final List moves = decodedData['moves'];
+      // getting the moves
+      final List moves = decodedData['moves'];
 
-    // getting the types
-    final List types = decodedData['types'];
+      // getting the types
+      final List types = decodedData['types'];
 
-    // getting the held items
-    final List heldItems = decodedData['held_items'];
+      // getting the held items
+      final List heldItems = decodedData['held_items'];
 
-    // updating the pokemon object
-    _pokemon = Pokemon(
-      id: id,
-      abilities: abilities,
-      attack: attack,
-      defense: defense,
-      hp: hp,
-      imageUrl: imageUrl,
-      moves: moves,
-      name: name,
-      specialAttack: specialAttack,
-      specialDefense: specialDefense,
-      speed: speed,
-      types: types,
-      weight: weight,
-      heldItems: heldItems,
-    );
-  }
-
-  Future<String> getDescription(String url) async {
-    // making the request
-    final http.Response response = await http.get(Uri.parse(url));
-
-    // decoding the response
-    final Map decodedResponse = jsonDecode(response.body);
-
-    // returning the first description
-    return (decodedResponse['flavor_text_entries'][0]['flavor_text'] as String)
-        .replaceAll('\n', ' ');
+      // updating the pokemon object
+      _pokemon = Pokemon(
+        id: id,
+        abilities: abilities,
+        attack: attack,
+        defense: defense,
+        hp: hp,
+        imageUrl: imageUrl,
+        moves: moves,
+        name: name,
+        specialAttack: specialAttack,
+        specialDefense: specialDefense,
+        speed: speed,
+        types: types,
+        weight: weight,
+        heldItems: heldItems,
+      );
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   // method to get results according to the search query
@@ -142,96 +138,116 @@ class PokemonProvider with ChangeNotifier {
 
   // method to load the ability description
   Future<String> loadAbilityDescription(String url) async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse(url),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse(url),
+      );
 
-    // decoding the response
-    final Map decodedResponse = jsonDecode(response.body);
+      // decoding the response
+      final Map decodedResponse = jsonDecode(response.body);
 
-    // getting all the entries
-    final List entries = decodedResponse['effect_entries'];
+      // getting all the entries
+      final List entries = decodedResponse['effect_entries'];
 
-    // returning the entry with english language because the response order is different each time
-    return entries.firstWhere(
-      (element) => element['language']['name'] == 'en',
-    )['effect'];
+      // returning the entry with english language because the response order is different each time
+      return entries.firstWhere(
+        (element) => element['language']['name'] == 'en',
+      )['effect'];
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   // method to load about
   Future<String> loadAbout(String pokemonName) async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse('https://pokeapi.co/api/v2/pokemon-species/$pokemonName'),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse('https://pokeapi.co/api/v2/pokemon-species/$pokemonName'),
+      );
 
-    // getting the body
-    final body = response.body;
+      // getting the body
+      final body = response.body;
 
-    // if response not found then let the user know
-    if (body.toLowerCase() == 'not found') {
-      return 'No about data found!';
-    }
-
-    // if there is response then work with it
-
-    // decoding the repsonse
-    final decodedResponse = jsonDecode(response.body);
-
-    // getting all the entries
-    final List entries = decodedResponse['flavor_text_entries'];
-
-    // returning the flavor text that is in english
-    for (int i = 0; i < entries.length; i++) {
-      if (entries[i]['language']['name'] == 'en') {
-        return entries[i]['flavor_text'].replaceAll('\n', ' ');
+      // if response not found then let the user know
+      if (body.toLowerCase() == 'not found') {
+        return 'No about data found!';
       }
-    }
 
-    // if no en entry found then return the first one
-    return entries[0]['flavor_text'].replaceAll('\n', ' ');
+      // if there is response then work with it
+
+      // decoding the repsonse
+      final decodedResponse = jsonDecode(response.body);
+
+      // getting all the entries
+      final List entries = decodedResponse['flavor_text_entries'];
+
+      // returning the flavor text that is in english
+      for (int i = 0; i < entries.length; i++) {
+        if (entries[i]['language']['name'] == 'en') {
+          return entries[i]['flavor_text'].replaceAll('\n', ' ');
+        }
+      }
+
+      // if no en entry found then return the first one
+      return entries[0]['flavor_text'].replaceAll('\n', ' ');
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   // method to load held item description
   Future<String> loadHeldItemDesc(String url) async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse(url),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse(url),
+      );
 
-    // decoding the response
-    final Map decodedResponse = jsonDecode(response.body);
+      // decoding the response
+      final Map decodedResponse = jsonDecode(response.body);
 
-    // returning the effect entry
-    return decodedResponse['effect_entries'][0]['effect'];
+      // returning the effect entry
+      return decodedResponse['effect_entries'][0]['effect'];
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   // this method will load the move description
   Future<void> loadMoveDesc(String url) async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse(url),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse(url),
+      );
 
-    // decoding the response
-    final Map decodedResponse = jsonDecode(response.body);
+      // decoding the response
+      final Map decodedResponse = jsonDecode(response.body);
 
-    // returning the move info
-    return decodedResponse['effect_entries'][0]['effect'];
+      // returning the move info
+      return decodedResponse['effect_entries'][0]['effect'];
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   // method to get the damage info of the type
   Future<Map> getTypeDamageInfo(String url) async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse(url),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse(url),
+      );
 
-    // decoding response
-    final Map decodedData = jsonDecode(response.body);
+      // decoding response
+      final Map decodedData = jsonDecode(response.body);
 
-    return decodedData;
+      return decodedData;
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   // method to find each pokemon's name and url from the chain
@@ -256,52 +272,60 @@ class PokemonProvider with ChangeNotifier {
 
   // method to load the chain
   Future<void> loadEvolutionData(String pokemonName) async {
-    // making the request to species data
-    final http.Response speciesResponse = await http.get(
-      Uri.parse('https://pokeapi.co/api/v2/pokemon-species/$pokemonName'),
-    );
+    try {
+      // making the request to species data
+      final http.Response speciesResponse = await http.get(
+        Uri.parse('https://pokeapi.co/api/v2/pokemon-species/$pokemonName'),
+      );
 
-    // clearing the previous data from the list
-    _pokemonsFromChain.clear();
+      // clearing the previous data from the list
+      _pokemonsFromChain.clear();
 
-    // if response is not found then just return
-    if (speciesResponse.body.toLowerCase() == 'not found') {
-      return;
+      // if response is not found then just return
+      if (speciesResponse.body.toLowerCase() == 'not found') {
+        return;
+      }
+
+      // decoding the response
+      final Map data = jsonDecode(speciesResponse.body);
+
+      // getting the evolution chain url
+      final String evolutionChainLink = data['evolution_chain']['url'];
+
+      // making the request to the evolution chain
+      final http.Response evolutionResponse = await http.get(
+        Uri.parse(evolutionChainLink),
+      );
+
+      // decoding the body
+      final Map chainData = jsonDecode(evolutionResponse.body);
+
+      // getting the evolvesTo
+      final Map evolvesTo = chainData['chain'];
+
+      // fetching the pokemons
+      fetchPokemonDataFromEvolutionChain(evolvesTo);
+    } catch (e) {
+      return Future.error(e);
     }
-
-    // decoding the response
-    final Map data = jsonDecode(speciesResponse.body);
-
-    // getting the evolution chain url
-    final String evolutionChainLink = data['evolution_chain']['url'];
-
-    // making the request to the evolution chain
-    final http.Response evolutionResponse = await http.get(
-      Uri.parse(evolutionChainLink),
-    );
-
-    // decoding the body
-    final Map chainData = jsonDecode(evolutionResponse.body);
-
-    // getting the evolvesTo
-    final Map evolvesTo = chainData['chain'];
-
-    // fetching the pokemons
-    fetchPokemonDataFromEvolutionChain(evolvesTo);
   }
 
   // method to get the info about a move
   Future<String> getMoveInfo(String url) async {
-    // making the get request
-    final http.Response response = await http.get(
-      Uri.parse(url),
-    );
+    try {
+      // making the get request
+      final http.Response response = await http.get(
+        Uri.parse(url),
+      );
 
-    // decoding the response
-    final Map decodedData = jsonDecode(response.body);
+      // decoding the response
+      final Map decodedData = jsonDecode(response.body);
 
-    // returning the move info
-    return decodedData['effect_entries'][0]['effect'];
+      // returning the move info
+      return decodedData['effect_entries'][0]['effect'];
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   // method to get names of the pokemons that are initially fetched
